6cf3f69cca15d9c26ac75ee336946bece983faea
WinteruOfficiel <Winteru@the-nwo.fun>
2023-05-11 01:11:54 +0200

    :heavy_plus_sign: Tests pour les CSV + Test automatique

---
+++ sae201/src/test/java/fr/uwu/CSVUtilsTest.java
@@ -0,0 +1,78 @@
+package fr.uwu;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+import java.util.List;
+
+import org.junit.Test;
+
+import fr.uwu.utils.CSVUtils;
+import fr.uwu.utils.ReadfileIterator;
+
+public class CSVUtilsTest {
+    
+    @Test
+    public void testReadFileIterator() throws IOException {
+        String strToTest = "123\n456\n789";
+        Reader br = new StringReader(strToTest);
+
+        ReadfileIterator rfi = new ReadfileIterator(br);
+
+        StringBuilder sb = new StringBuilder();
+        int nbLignes = 0;
+
+        while (rfi.hasNext()) {
+            String ligne = rfi.next();
+            sb.append(ligne);
+            if (rfi.hasNext()) {
+                sb.append("\n");
+            }
+            nbLignes++;
+        }
+
+        rfi.close();
+
+        assert(nbLignes == 3);
+        assert(sb.toString().equals(strToTest));
+    }
+
+    @Test
+    public void testReadFileWithClassLoader(){
+        ClassLoader classLoader = this.getClass().getClassLoader();
+        File file = new File(classLoader.getResource("lorem_ipsum.txt").getFile());
+        assert(file.exists());
+
+    }
+
+     @Test
+    public void testReadStation() {
+        List<Station> stations = CSVUtils.readStationCSV(getClass().getClassLoader().getResource("stations.csv").getPath());
+
+        Station station1 = stations.get(0);
+
+        System.out.println(station1);
+
+        assert(stations.size() == 376);
+        assert(station1.id == 0);
+        assert(station1.nom.equals("Abbesses"));
+        assert(station1.ligne.equals("12"));
+        assert(station1.terminus == false);
+    }
+
+    @Test
+    public void testReadRelation() {
+        List<Relation> relations = CSVUtils.readRelationCSV(getClass().getClassLoader().getResource("relations.csv").getPath());
+
+        Relation relation1 = relations.get(0);
+
+        // Ligne 1 du CSV : 
+        // id1;id2;temps
+        // 0;238;41
+
+        assert(relation1.id1 == 0);
+        assert(relation1.id2 == 238);
+        assert(relation1.temps == 41);
+    } 
+}
